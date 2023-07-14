3bb6c316292dd90006b33eb893a9c72798b334ca
innocent259 <ndahimanainnocent842@gmail.com>
2023-07-14 08:14:39 +0200

    Create navigation bar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>Book store</h1>
+      <ul>
+        <li>Books</li>
+        <li>Categories</li>
+      </ul>
+      <div>
+        <p>Img</p>
+      </div>
+    </nav>
+  )
+}
