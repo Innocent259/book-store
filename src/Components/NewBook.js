0460520afa378f581e21850574b71f96d2740841
innocent259 <ndahimanainnocent842@gmail.com>
2023-07-14 14:42:19 +0200

    Add book form to add new book

--- src/Components/NewBook.js
+++ src/Components/NewBook.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function NewBook() {
+  const [bookData, setBookData] = useState({
+    book: '',
+    author: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setBookData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setBookData({
+      book: '',
+      author: '',
+    });
+  };
+
   return (
     <div>
-      <p>this will display add book form</p>
+      <form className="form-container" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="book"
+          value={bookData.book}
+          placeholder="Book title"
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="author"
+          value={bookData.author}
+          placeholder="Author"
+          onChange={handleInputChange}
+        />
+        <button type="submit">Add book</button>
+      </form>
     </div>
   );
 }
