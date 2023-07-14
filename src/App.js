99c93664160db64979a41b132f4c8d179870db38
innocent259 <ndahimanainnocent842@gmail.com>
2023-07-14 14:38:17 +0200

    Use React Routers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import BookApp from './Components/BookApp';
+import Navbar from './Components/Navbar';
+import Categories from './Components/Categories';
 
 function App() {
   return (
     <div className="App">
-      <BookApp />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<BookApp />} />
+        <Route path="categories" element={<Categories />} />
+      </Routes>
     </div>
   );
 }
