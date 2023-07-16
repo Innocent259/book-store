df1c84e8b8d5c806716227bb02ae54cf4272b88b
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-07-17 00:36:25 +0200

    Modify the navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,16 +3,30 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <nav className="navigation">
-      <h1>Book store</h1>
-      <ul className="navigation-list">
-        <li>
-          <Link className="link" to="/">Quote</Link>
-        </li>
-        <li>
-          <Link className="link" to="/categories">Categories</Link>
-        </li>
-      </ul>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-lg">
+        <Link className="navbar-brand" to="/">
+          <h1 className="text-primary">Bookstore CMS</h1>
+        </Link>
+        <button className="navbar-toggler d-md-none" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link active" to="/">Quote</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/categories">Categories</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="d-none d-md-block">
+          right-img
+        </div>
+      </div>
     </nav>
   );
 }
