59ee13dd06c34a5b51543d2e76aa5b151b5d240d
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-07-18 18:48:47 +0200

    Connect store to bookdetails component

--- src/Components/BookContainer.js
+++ src/Components/BookContainer.js
@@ -1,29 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import BookDetails from './BookDetails';
 
 export default function BookContainer() {
-  const booksData = [
-    {
-      id: 1,
-      title: 'The Hunger Games',
-      author: 'Suzanne Collins',
-    },
-    {
-      id: 2,
-      title: 'Dune',
-      author: 'Frank Herbert',
-    },
-    {
-      id: 3,
-      title: 'Capital in the Twenty First Century',
-      author: 'Suzanne Collins',
-    },
-  ];
+  const { books } = useSelector((state) => state.books);
+
   return (
     <>
       <ul>
-        { booksData.map((book) => (
-          <BookDetails key={book.id} book={book} />
+        {books.map((book) => (
+          <BookDetails
+            key={book.id}
+            id={book.id}
+            title={book.title}
+            author={book.author}
+          />
         ))}
       </ul>
     </>
