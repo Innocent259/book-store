b81a8b53b0beb732e635c8b432c78a1dcd19541f
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2023-07-18 18:51:15 +0200

    Connect redux to BookDetails component

--- src/Components/BookDetails.js
+++ src/Components/BookDetails.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import 'bootstrap/dist/css/bootstrap.css';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { removeBook } from '../redux/books/booksSlice';
 
-export default function BookDetails({ book }) {
+export default function BookDetails({ id, title, author }) {
+  const dispatch = useDispatch();
+
+  const handleRemoveBook = () => {
+    dispatch(removeBook(id));
+  };
+
   return (
     <li>
       <div className="row">
         <div className="col-md-6">
-          <h2>{book.title}</h2>
-          <h5>{book.author}</h5>
+          <h2>{title}</h2>
+          <h5>{author}</h5>
           <span>
-            <button className="btn btn-primary" type="button">Comment</button>
-            <button className="btn btn-danger" type="button">Remove</button>
-            <button className="btn btn-info" type="button">Edit</button>
+            <button className="btn btn-primary" type="button">
+              Comment
+            </button>
+            <button
+              className="btn btn-danger"
+              type="button"
+              onClick={handleRemoveBook}
+            >
+              Remove
+            </button>
           </span>
         </div>
         <div className="col-md-6">
           <div className="right-content">
             <h3>Current Chapter</h3>
             <p>Chapter 7</p>
-            <button className="btn btn-info" type="button">Update Progress</button>
+            <button className="btn btn-info" type="button">
+              Update Progress
+            </button>
           </div>
         </div>
       </div>
@@ -27,9 +44,7 @@ export default function BookDetails({ book }) {
   );
 }
 BookDetails.propTypes = {
-  book: PropTypes.shape({
-    category: PropTypes.string.isRequired,
-    author: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-  }).isRequired,
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
 };
